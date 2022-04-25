6cfca6838f4739083f7b6d5c67d552054cb23a98
sujl95 <sujl95@naver.com>
2022-04-26 02:38:27 +0900

    1. JDBC 이해, JDBC 개발 - 조회

--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -7,6 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.NoSuchElementException;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -35,6 +36,36 @@ public class MemberRepositoryV0 {
       pstmt.close();
       con.close();
     }
+  }
+
+  public Member findById(String memberId) throws SQLException {
+    String sql = "select * from member where member_id  = ?";
+
+    Connection con = null;
+    PreparedStatement pstmt = null;
+    ResultSet rs = null;
+    try {
+      con = getConnection();
+      pstmt = con.prepareStatement(sql);
+      pstmt.setString(1, memberId);
+      rs = pstmt.executeQuery();
+
+      if (rs.next()) {
+        Member member = new Member();
+        member.setMemberId(rs.getString("member_id"));
+        member.setMoney(rs.getInt("money"));
+        return member;
+      } else {
+        throw new NoSuchElementException("member not found memberId=" + memberId);
+      }
+
+    } catch (SQLException e) {
+      log.error("db error", e);
+      throw e;
+    } finally {
+      close(con, pstmt, rs);
+    }
+
   }
 
   private void close(Connection con, Statement stmt, ResultSet rs) {
--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,11 +1,14 @@
 package hello.jdbc.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import hello.jdbc.domain.Member;
 import java.sql.SQLException;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
   MemberRepositoryV0 repository = new MemberRepositoryV0();
@@ -15,6 +18,11 @@ class MemberRepositoryV0Test {
     // save
     Member member = new Member("memberV0", 10_000);
     repository.save(member);
+
+    //findById
+    Member findMember = repository.findById(member.getMemberId());
+    log.info("findMember= {}", findMember);
+    assertThat(findMember).isEqualTo(member);
 
   }
 
