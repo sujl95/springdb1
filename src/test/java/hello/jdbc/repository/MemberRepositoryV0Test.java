3642237c288c3f22a82390199262be91e5108754
sujl95 <sujl95@naver.com>
2022-04-26 02:21:52 +0900

    1. JDBC 이해, JDBC 개발 - 등록

---
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -0,0 +1,21 @@
+package hello.jdbc.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import hello.jdbc.domain.Member;
+import java.sql.SQLException;
+import org.junit.jupiter.api.Test;
+
+class MemberRepositoryV0Test {
+
+  MemberRepositoryV0 repository = new MemberRepositoryV0();
+
+  @Test
+  void crud() throws SQLException {
+    // save
+    Member member = new Member("memberV0", 10_000);
+    repository.save(member);
+
+  }
+
+}
