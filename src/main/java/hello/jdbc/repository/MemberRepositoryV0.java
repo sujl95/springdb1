5dd7648977a2a5ca7bac9e33132babbf9686e1dc
sujl95 <sujl95@naver.com>
2022-04-26 02:50:38 +0900

    1. JDBC 이해, JDBC 개발 - 수정, 삭제

--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -38,6 +38,42 @@ public class MemberRepositoryV0 {
     }
   }
 
+  public void delete(String memberId) throws SQLException {
+    String sql = "delete from member where member_id=?";
+    Connection con = null;
+    PreparedStatement pstmt = null;
+    try {
+      con = getConnection();
+      pstmt = con.prepareStatement(sql);
+      pstmt.setString(1, memberId);
+      pstmt.executeUpdate();
+    } catch (SQLException e) {
+      log.error("db error", e);
+      throw e;
+    } finally {
+      close(con, pstmt, null);
+    }
+  }
+
+  public void update(String memberId, int money) throws SQLException {
+    String sql = "update member set money=? where member_id=?";
+    Connection con = null;
+    PreparedStatement pstmt = null;
+    try {
+      con = getConnection();
+      pstmt = con.prepareStatement(sql);
+      pstmt.setInt(1, money);
+      pstmt.setString(2, memberId);
+      int resultSize = pstmt.executeUpdate();
+      log.info("resultSize={}", resultSize);
+    } catch (SQLException e) {
+      log.error("db error", e);
+      throw e;
+    } finally {
+      close(con, pstmt, null);
+    }
+  }
+
   public Member findById(String memberId) throws SQLException {
     String sql = "select * from member where member_id  = ?";
 
--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,10 +1,11 @@
 package hello.jdbc.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import hello.jdbc.domain.Member;
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -16,7 +17,7 @@ class MemberRepositoryV0Test {
   @Test
   void crud() throws SQLException {
     // save
-    Member member = new Member("memberV0", 10_000);
+    Member member = new Member("memberV1", 10_000);
     repository.save(member);
 
     //findById
@@ -24,6 +25,15 @@ class MemberRepositoryV0Test {
     log.info("findMember= {}", findMember);
     assertThat(findMember).isEqualTo(member);
 
+    //update: money 10000 -> 20000
+    repository.update(member.getMemberId(), 20000);
+    Member updatedMember = repository.findById(member.getMemberId());
+    assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+    //delete
+    repository.delete(member.getMemberId());
+    assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+        .isInstanceOf(NoSuchElementException.class);
   }
 
 }
