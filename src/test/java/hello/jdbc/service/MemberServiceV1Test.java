f33b336ab700dae133872f5733d242661273ca44
sujl95 <sujl95@naver.com>
2022-05-31 20:06:27 +0900

    트랜잭션 이해 - 트랜잭션 - 적용1

--- src/test/java/hello/jdbc/service/MemberServiceV1Test.java
+++ src/test/java/hello/jdbc/service/MemberServiceV1Test.java
@@ -4,11 +4,12 @@ import static hello.jdbc.connection.ConnectionConst.PASSWORD;
 import static hello.jdbc.connection.ConnectionConst.URL;
 import static hello.jdbc.connection.ConnectionConst.USERNAME;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV1;
 import java.sql.SQLException;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -33,6 +34,13 @@ class MemberServiceV1Test {
     memberService = new MemberServiceV1(memberRepository);
   }
 
+  @AfterEach
+  void after() throws SQLException {
+    memberRepository.delete(MEMBER_A);
+    memberRepository.delete(MEMBER_B);
+    memberRepository.delete(MEMBER_EX);
+  }
+
   @Test
   @DisplayName("정상 이체")
   void accountTransfer() throws SQLException {
@@ -50,6 +58,31 @@ class MemberServiceV1Test {
     Member findMemberB = memberRepository.findById(memberB.getMemberId());
     assertThat(findMemberA.getMoney()).isEqualTo(8_000);
     assertThat(findMemberB.getMoney()).isEqualTo(12_000);
+  }
+
+  @Test
+  @DisplayName("이체중 예외 발생")
+  void accountTransferEx() throws SQLException {
+    //given
+    Member memberA = new Member(MEMBER_A, 10_000);
+    Member memberEx = new Member(MEMBER_EX, 10_000);
+    memberRepository.save(memberA);
+    memberRepository.save(memberEx);
+
+    //when
+    assertThatThrownBy(() ->
+        memberService.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(),
+            2000))
+        .isInstanceOf(IllegalStateException.class);
+
+    //then
+    Member findMemberA = memberRepository.findById(memberA.getMemberId());
+    Member findMemberEx =
+        memberRepository.findById(memberEx.getMemberId());
+
+    //memberA의 돈만 2000원 줄었고, ex의 돈은 10000원 그대로이다.
+    assertThat(findMemberA.getMoney()).isEqualTo(8_000);
+    assertThat(findMemberEx.getMoney()).isEqualTo(10_000);
   }
 
 }
